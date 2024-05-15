4851d070d73f6253469d95cd06468f612aa31786
lanav85 <lanaviana.o@gmail.com>
2024-05-15 18:01:27 -0300

    Updated countweekdays

--- fullstack-backend/src/main/java/com/HolidayTracker/fullstackbackend/service/ValidationLogic.java
+++ fullstack-backend/src/main/java/com/HolidayTracker/fullstackbackend/service/ValidationLogic.java
@@ -4,16 +4,21 @@ import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 @Component
 public class ValidationLogic {
 
     public long countWeekdays(Date requestFrom, Date requestTo) {
         Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTimeZone(TimeZone.getTimeZone("UTC"));
         startCalendar.setTime(requestFrom);
 
         Calendar endCalendar = Calendar.getInstance();
+        endCalendar.setTimeZone(TimeZone.getTimeZone("UTC"));
         endCalendar.setTime(requestTo);
+
+
 
         long weekdays = 0;
         // Iterate through each day between the start and end dates
