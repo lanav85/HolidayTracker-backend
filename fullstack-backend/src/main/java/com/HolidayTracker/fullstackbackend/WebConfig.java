e81cb849ad4c16738dfa1c7ef0d9847be225f034
lanav85 <lanaviana.o@gmail.com>
2024-07-03 22:57:56 +0100

    added netlify to the mapping

--- fullstack-backend/src/main/java/com/HolidayTracker/fullstackbackend/WebConfig.java
+++ fullstack-backend/src/main/java/com/HolidayTracker/fullstackbackend/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                //connected it to nelify as well
                 .allowedOrigins("https://myholidaytracker.netlify.app/", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
